d23bcb148ed8fc74d8cb935f4c7fd5701f2a4491
assitburn <87494405+assitburn@users.noreply.github.com>
2021-08-21 11:34:44 +0200

    Fehlerbeseitigt
    Fehlerbeseitigung Resize

--- js/size.js
+++ js/size.js
@@ -30,79 +30,145 @@ async function bildgroesseaendern(px){
     const activeDoc = app.activeDocument; 
     
     if (activeDoc.height > activeDoc.width){
-        let ausrichtung = "height";
+        const batchPlay = require("photoshop").action.batchPlay;
+        const result = await batchPlay(
+        [
+        {
+            "_obj": "invokeCommand",
+            "commandID": 1030,
+            "kcanDispatchWhileModal": true,
+            "_isCommand": false,
+            "_options": {
+                "dialogOptions": "dontDisplay"
+            }
+        },
+        {
+            "_obj": "modalStateChanged",
+            "level": 1,
+            "state": {
+                "_enum": "state",
+                "_value": "enter"
+            },
+            "kcanDispatchWhileModal": true,
+            "title": "Image Size",
+            "_isCommand": false,
+            "_options": {
+                "dialogOptions": "dontDisplay"
+            }
+        },
+        {
+            "_obj": "modalStateChanged",
+            "level": 0,
+            "state": {
+                "_enum": "state",
+                "_value": "exit"
+            },
+            "kcanDispatchWhileModal": true,
+            "title": "Image Size",
+            "_isCommand": false,
+            "_options": {
+                "dialogOptions": "dontDisplay"
+            }
+        },
+        {
+            "_obj": "imageSize",
+            "height": {
+                "_unit": "pixelsUnit",
+                "_value": px
+            },
+            "scaleStyles": true,
+            "constrainProportions": true,
+            "interfaceIconFrameDimmed": {
+                "_enum": "interpolationType",
+                "_value": "automaticInterpolation"
+            },
+            "_isCommand": true,
+            "_options": {
+                "dialogOptions": "dontDisplay"
+            }
+        }
+      
+        ],{
+        "synchronousExecution": false,
+        
+        "historyStateInfo": {
+            "name": label_bildgroesse + px + label_pixel,
+            "target": {
+            "_ref":"document",
+            "_enum": "ordinal",
+            "_value": "targetEnum"
+            }}
+        });
     }else{
-        let ausrichtung = "width";
+        const batchPlay = require("photoshop").action.batchPlay;
+        const result = await batchPlay(
+        [
+        {
+            "_obj": "invokeCommand",
+            "commandID": 1030,
+            "kcanDispatchWhileModal": true,
+            "_isCommand": false,
+            "_options": {
+                "dialogOptions": "dontDisplay"
+            }
+        },
+        {
+            "_obj": "modalStateChanged",
+            "level": 1,
+            "state": {
+                "_enum": "state",
+                "_value": "enter"
+            },
+            "kcanDispatchWhileModal": true,
+            "title": "Image Size",
+            "_isCommand": false,
+            "_options": {
+                "dialogOptions": "dontDisplay"
+            }
+        },
+        {
+            "_obj": "modalStateChanged",
+            "level": 0,
+            "state": {
+                "_enum": "state",
+                "_value": "exit"
+            },
+            "kcanDispatchWhileModal": true,
+            "title": "Image Size",
+            "_isCommand": false,
+            "_options": {
+                "dialogOptions": "dontDisplay"
+            }
+        },
+        {
+            "_obj": "imageSize",
+            "width": {
+                "_unit": "pixelsUnit",
+                "_value": px
+            },
+            "scaleStyles": true,
+            "constrainProportions": true,
+            "interfaceIconFrameDimmed": {
+                "_enum": "interpolationType",
+                "_value": "automaticInterpolation"
+            },
+            "_isCommand": true,
+            "_options": {
+                "dialogOptions": "dontDisplay"
+            }
+        }
+      
+        ],{
+        "synchronousExecution": false,
+        
+        "historyStateInfo": {
+            "name": label_bildgroesse + px + label_pixel,
+            "target": {
+            "_ref":"document",
+            "_enum": "ordinal",
+            "_value": "targetEnum"
+            }}
+        });
     }
-    
-    const batchPlay = require("photoshop").action.batchPlay;
-    const result = await batchPlay(
-    [
-    {
-        "_obj": "invokeCommand",
-        "commandID": 1030,
-        "kcanDispatchWhileModal": true,
-        "_isCommand": false,
-        "_options": {
-            "dialogOptions": "dontDisplay"
-        }
-    },
-    {
-        "_obj": "modalStateChanged",
-        "level": 1,
-        "state": {
-            "_enum": "state",
-            "_value": "enter"
-        },
-        "kcanDispatchWhileModal": true,
-        "title": "Image Size",
-        "_isCommand": false,
-        "_options": {
-            "dialogOptions": "dontDisplay"
-        }
-    },
-    {
-        "_obj": "modalStateChanged",
-        "level": 0,
-        "state": {
-            "_enum": "state",
-            "_value": "exit"
-        },
-        "kcanDispatchWhileModal": true,
-        "title": "Image Size",
-        "_isCommand": false,
-        "_options": {
-            "dialogOptions": "dontDisplay"
-        }
-    },
-    {
-        "_obj": "imageSize",
-        "height": {
-            "_unit": "pixelsUnit",
-            "_value": px
-        },
-        "scaleStyles": true,
-        "constrainProportions": true,
-        "interfaceIconFrameDimmed": {
-            "_enum": "interpolationType",
-            "_value": "automaticInterpolation"
-        },
-        "_isCommand": true,
-        "_options": {
-            "dialogOptions": "dontDisplay"
-        }
-    }
-  
-    ],{
-    "synchronousExecution": false,
-    
-    "historyStateInfo": {
-        "name": label_bildgroesse + px + label_pixel,
-        "target": {
-        "_ref":"document",
-        "_enum": "ordinal",
-        "_value": "targetEnum"
-        }}
-    });
     await menuCommand(1192);
 }
